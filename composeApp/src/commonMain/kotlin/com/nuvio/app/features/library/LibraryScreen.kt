31e94520f14544d0e4e7e3f324c47efbd409255a
Luqman Fadlli <luqman.fadlli@gmail.com>
2026-09-16 05:39:28 +0700

    fix(library): restore LaunchedEffect import after the 0.4.21 merge

--- composeApp/src/commonMain/kotlin/com/nuvio/app/features/library/LibraryScreen.kt
+++ composeApp/src/commonMain/kotlin/com/nuvio/app/features/library/LibraryScreen.kt
@@ -65,6 +65,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.foundation.gestures.stopScroll
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
